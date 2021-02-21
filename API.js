3467549747db963b6fa89f24f1850d8afab72841
AustinCapaviella <amcapaviella@gmail.com>
2021-02-20 18:31:20 -0800

    Refactor filter function to take user inputs

--- API.js
+++ API.js
@@ -37,12 +37,21 @@ const findInProgress = (games) => {
     return inProgress;
 }
 
+
+const userInputs = {
+    homeFouls: 1,
+    awayFouls: 1,
+    minutesRemaining: 12,
+    scoreDifferential: [3, 8]
+}
+
+
 //returns all games that have high number of fouls 
 const findProspects = (liveGames) => {
     const prospects = []
     for (var i = 0; i < liveGames.length; i++) {
         const gameData = grabGameData(liveGames[i])
-        const checkedGame = filterHighFouls(gameData);
+        const checkedGame = filterGames(gameData, userInputs);
         if (checkedGame === true) {
             prospects.push([gameData.homeTeam, gameData.homeFouls, gameData.awayTeam, gameData.awayFouls, gameData.minuteRemaining]);
         }
@@ -54,23 +63,30 @@ const findProspects = (liveGames) => {
 //returns data object with game data (teams, fouls, time remaining)
 const grabGameData = (game) => {
     const box = game["TeamGames"];
+    const gameData = game["Game"];
     const homeTeam = box[0]["Name"];
     const awayTeam = box[1]["Name"];
     const homeFouls = box[0]["PersonalFouls"];
     const awayFouls = box[1]["PersonalFouls"];
+    const awayScore = gameData["AwayTeamScore"];
+    const homeScore = gameData["HomeTeamScore"];
     const minuteRemaining = game["Game"]["TimeRemainingMinutes"];
-    let data = { homeTeam: homeTeam, awayTeam: awayTeam, homeFouls: homeFouls, awayFouls: awayFouls, minuteRemaining: minuteRemaining };
+
+    let data = { homeTeam: homeTeam, awayTeam: awayTeam, homeFouls: homeFouls, awayFouls: awayFouls, minuteRemaining: minuteRemaining, awayScore: awayScore, homeScore: homeScore };
     return data
 }
 
+
+
 //filters all games where fouls are greater than input - right now using 1 because sample data fouls are all low numbers - should be 5 or 6
-const filterHighFouls = (gameData) => {
-    if (gameData.homeFouls > 1 && gameData.awayFouls > 1) {
+const filterGames = (gameData, userInputs) => {
+    if (gameData.homeFouls > userInputs.homeFouls && gameData.awayFouls > userInputs.awayFouls) {
         return true
     } else {
         return false
     }
 }
+
 
 //calls fetchData and stores all betting prospects 
 const finalProspects = fetchData(url)
