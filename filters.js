e33a8a516d3c78d3ba99325d90a95b33dd5f4db1
AustinCapaviella <amcapaviella@gmail.com>
2021-02-22 15:43:57 -0800

    Remove filtering out of API.js into filters.js
    User will be able to choose their own
    and apply them to the data

--- filters.js
+++ filters.js
@@ -1,33 +1,3 @@
-const axios = require('axios');
-const key = require('./keys.js');
-
-//formats date for use in the url string below
-const formatDate = (date) => {
-    return date.substring(11, 15) + "-" + date.substring(4, 7) + "-" + date.substring(8, 10)
-}
-
-let date = formatDate(Date())
-
-const url = `https://api.sportsdata.io/v3/cbb/stats/json/BoxScores/${date}`
-
-//axios config
-let config = {
-    headers: {
-        "Ocp-Apim-Subscription-Key": key
-    }
-}
-
-//fetches college basketball data from sportsdata api and returns all games currently in progress
-const fetchData = async (url) => {
-    const myGames = await axios(url, config).
-        then((results) => {
-            return findInProgress(results.data);
-        }).catch((error) => {
-            console.log(error)
-        })
-    return myGames
-}
-
 //returns all games currently in progress
 const findInProgress = (games) => {
     let inProgress = []
@@ -90,14 +60,3 @@ const filterGames = (gameData, userInputs) => {
         return false
     }
 }
-
-
-// calls fetchData and stores all betting prospects
-const finalProspects = fetchData(url).then((data) => {
-    console.log(data)
-    console.log('There are ' + data.length + ' live games')
-})
-
-
-
-
