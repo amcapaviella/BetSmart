c78a70eb455d9f0dba6404d26c15b3946c3c2253
AustinCapaviella <amcapaviella@gmail.com>
2021-02-22 20:41:32 -0800

    Add liveGames state and UseEffect to App.js
    setInterval fetch request now inside of our App.js
    useEffect function. This way it fires only once on load
    but continues to pull data every 15 seconds

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchData, url } from "../../server/API"
+
 
 
 const App = () => {
-    return ('Welcome to your app');
+    const [liveGames, setLiveGames] = useState([])
+
+    //fetches college basketball live scores every 15 seconds 
+
+    const updateLiveGames = (games) => {
+        setLiveGames()
+    }
+
+    useEffect(() => {
+        setInterval(() => {
+            fetchData(url).then((data) => {
+                console.log('there are ' + data.length + ' games live')
+                updateLiveGames(data)
+            }).then(() => {
+                console.log(liveGames)
+            })
+        }, 15000)
+    }, [])
+
+    return (
+        <div>
+            {liveGames}
+        </div>
+    )
 }
 
 export default App
