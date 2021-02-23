674906c6163526cc4e3511f4b65d441aac1e4cf7
AustinCapaviella <amcapaviella@gmail.com>
2021-02-22 20:03:27 -0800

    Add Webpack and Babel config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+    entry: `${SRC_DIR}/index.js`,
+    output: {
+        filename: 'bundle.js',
+        path: DIST_DIR
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                loader: 'babel-loader',
+                options: {
+                    'presets': ['@babel/preset-env', '@babel/preset-react']
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
+            }
+        ]
+    }
+};
